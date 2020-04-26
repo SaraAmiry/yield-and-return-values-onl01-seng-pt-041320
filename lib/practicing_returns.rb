7e816549de12365aaa51c49492c716ce2fa6c0f9
Sara Amiry <saraamiry24@gmail.com>
2020-04-26 10:42:26 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,7 +3,8 @@ require 'pry'
 def hello(array)
   i = 0
   collection = []
-  while i < array.length collection << yiled(array[i])
+  while i < array.length 
+  collection << yiled(array[i])
     i += 1
   end
   collection
